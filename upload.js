763b33d0a8b226a465c51fedfac6d43670205992
Ghordrin <55690290+Ghordrin@users.noreply.github.com>
2023-03-24 11:46:20 +0100

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('uploadForm');
+const fileList = document.getElementById('fileList');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const fileInput = document.getElementById('file');
+	const file = fileInput.files[0];
+	const formData = new FormData();
+	formData.append('file', file);
+
+	fetch('https://api.github.com/repos/ghordrin/dndwebsite/contents/PATH_TO_UPLOAD_DIRECTORY/' + file.name, {
+		method: 'PUT',
+		headers: {
+			'Authorization': 'token ACCESS_TOKEN',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			message: 'Upload file',
+			content: btoa(file),
+			sha: ''
+		})
+	})
+	.then(response => response.json())
+	.then(data => {
+		const fileName = data.content.name;
+		const fileURL = data.content.html_url;
+		const listItem = document.createElement('li');
+		const link = document.createElement('a');
+		link.href = fileURL;
+		link.textContent = fileName;
+		listItem.appendChild(link);
+		fileList.appendChild(listItem);
+	})
+	.catch(error => console.error(error));
+});
